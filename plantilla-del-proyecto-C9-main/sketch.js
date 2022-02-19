8d00fa76cc89612595f9495b4426736c399d0210
EivorProgamer <100003833+EivorProgamer@users.noreply.github.com>
2022-02-18 18:49:15 -0600

    Add files via upload

---
+++ plantilla-del-proyecto-C9-main/sketch.js
@@ -0,0 +1,40 @@
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+
+  // escribir el código para cambiar el color del fondo 
+  // rojo cuando se presiona RIGHT_ARROW (tecla de flecha derecha)
+  if (keyIsDown(RIGHT_ARROW)){
+    background("red")
+  }
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+
+  
+  drawSprites();
+}
+
